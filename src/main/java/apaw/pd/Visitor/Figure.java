0dd0a1a081ecd4ce3c20ba1546c7f9cfe8e50b95
Thomas Loor <pipos_rgt@hotmail.com>
2016-10-10 13:10:34 -0500

    Start Visitor

---
+++ src/main/java/apaw/pd/Visitor/Figure.java
@@ -0,0 +1,11 @@
+package apaw.pd.Visitor;
+
+public abstract class Figure {
+
+    public Figure(String description) {
+    }
+    
+    public abstract double area();
+    public abstract double numberOfSides();
+
+}
